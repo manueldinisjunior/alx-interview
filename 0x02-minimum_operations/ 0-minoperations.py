d80ce4ed8947971bc240f1cb8a492c2852f43ca1
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-10 08:05:49 +0200

    Mini

--- 0x02-minimum_operations/ 0-minoperations.py
+++ 0x02-minimum_operations/ 0-minoperations.py
@@ -1,21 +1,50 @@
 #!/usr/bin/python3
 """
-Contains a method that calculates the fewest number of operations
-needed to calculate charater H.
+a method calculates the fewest number of operations
+needed to result in exactly n H character in the file
 """
+
+
+def isPrime(number):
+    """
+    the function checks is a number is a prime
+    """
+    if number < 2:
+        return False
+
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
 def minOperations(n):
     """
-    Returns the fewest number of operations needed to result in exactly
-    n H characters in the file.
+    the method calculating the fewest number of operations needed
     """
-    operations = 0
-    min_operations = 2
-    while n > 1:
-        while n % min_operations == 0:
-            operations += min_operations
-            n /= min_operations
-        min_operations += 1
-    return operations
-    
+    prime_factors = []
+    div = 2
+
+    if not str(n).isdigit():
+        return 0
+
+    if n == 1:
+        return 1
+    elif (n < 1):
+        return 0
+
+    while True:
+        if n % div == 0:
+            prime_factors.append(div)
+            n = int(n/div)
+        else:
+            while True:
+                div += 1
+                if (isPrime(div)):
+                    break
+
+        if n == 1:
+            break
+
+    minOps = sum([prime for prime in prime_factors])
+    return(minOps)
