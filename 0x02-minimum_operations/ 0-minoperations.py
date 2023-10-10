5ab2215f7e90c42dabd0318866944d512f5a0218
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-10 07:56:26 +0200

    Mini

--- 0x02-minimum_operations/ 0-minoperations.py
+++ 0x02-minimum_operations/ 0-minoperations.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-""" 
-Script that computes a minimum operations
-"""
-def minOperations(n):
+""" Minimum Operations
     """
-    Method for compute the minimum number
-    of operations for task
 
-    Args:
-        n: input value
-        factor_list: List to save the operations
-    Return: the sum of the operations
-    """
-    if n < 2:
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-    factor_list = []
-    i = 1
-    while n != 1:
-        i += 1
-        if n % i == 0:
-            while n % i == 0:
-                n /= i
-                factor_list.append(i)
-    return sum(factor_list)
+    return op
+    
