ceaebcc2b6946a5c30e1917a6932574a968f4e1b
Manuel Dinis Júnior <91975829+manueldinisjunior@users.noreply.github.com>
2023-10-23 07:55:09 +0200

    ALX Interviw UTF8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,39 +1,29 @@
 #!/usr/bin/python3
-"""UTF-8 Validation"""
+"""
+    Check if a set of integers are valid utf-8 format
+    data = [1,2,3 ..., n]
+    return True if all integers in list are valid utf-8 format
+    Otherwise False
+"""
 
 
 def validUTF8(data):
-    """Determines if a given data set
-    represents a valid utf-8 encoding
-    """
-    number_bytes = 0
-
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
-
-    for i in data:
-
-        mask_byte = 1 << 7
-
-        if number_bytes == 0:
-
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
-                continue
-
-            if number_bytes == 1 or number_bytes > 4:
+    """Check if a set of integers are valid utf-8 format"""
+    num_bytes = 0
+    for char_d in data:
+        byte = char_d & 0xff
+        if num_bytes:
+            if (byte >> 6 == 1 or byte >> 6 == 3):
                 return False
+            num_bytes -= 1
+            continue
 
-        else:
-            if not (i & mask_1 and not (i & mask_2)):
-                return False
+        while 7 - num_bytes and byte & (1 << (7 - num_bytes)):
+            num_bytes += 1
+        if num_bytes == 1 or num_bytes > 4:
+            return False
+        num_bytes = max(num_bytes - 1, 0)
 
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False
+    if num_bytes:
+        return False
+    return True
